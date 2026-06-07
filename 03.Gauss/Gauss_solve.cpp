e4f2744b11e5c744ba82c795ce9fcd3d6418170e
Danilov Egor <st141853@student.spbu.ru>
2026-06-07 17:08:51 +0300

    Add realisation of method

--- 03.Gauss/Gauss_solve.cpp
+++ 03.Gauss/Gauss_solve.cpp
@@ -1,6 +1,35 @@
 #include "Gauss_solve.h"
+#include <cmath>
 
 GaussVector Gauss_solve(GaussMatrix &ab)
 {
-    return GaussVector(ab.rows());
+    const Eigen::Index n = ab.rows();
+
+    for (Eigen::Index k = 0; k < n; ++k)
+    {
+        Eigen::Index piv = k;
+        for (Eigen::Index i = k + 1; i < n; ++i)
+            if (std::abs(ab(i, k)) > std::abs(ab(piv, k)))
+                piv = i;
+
+        if (piv != k)
+            ab.row(k).swap(ab.row(piv));
+
+        for (Eigen::Index i = k + 1; i < n; ++i)
+        {
+            double f = ab(i, k) / ab(k, k);
+            ab.row(i) -= f * ab.row(k);
+        }
+    }
+
+    GaussVector x(n);
+    for (Eigen::Index i = n - 1; i >= 0; --i)
+    {
+        double s = ab(i, n);
+        for (Eigen::Index j = i + 1; j < n; ++j)
+            s -= ab(i, j) * x(j);
+        x(i) = s / ab(i, i);
+    }
+
+    return x;
 }
